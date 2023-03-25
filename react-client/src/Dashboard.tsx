aefc4d7583dce7102aeca98d0b10283115543a7c
Pierre Hubert <pierre.git@communiquons.org>
2023-03-25 18:12:32 +0100

    Add basic Dashboard

--- react-client/src/Dashboard.tsx
+++ react-client/src/Dashboard.tsx
@@ -1,26 +1,58 @@
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Box,
+  Tab,
+} from "@mui/material";
 import React from "react";
+import { TasksStatus } from "./TasksStatus";
 
-export class Dashboard extends React.Component<{
-  token: string;
-  onLogout: () => any;
-}> {
+export class Dashboard extends React.Component<
+  {
+    token: string;
+    onLogout: () => any;
+  },
+  { tab: "status" | "task-manager" }
+> {
   constructor(p: any) {
     super(p);
+    this.state = { tab: "status" };
   }
 
   render(): React.ReactNode {
     return (
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            CometRender
-          </Typography>
-          <Button color="inherit" onClick={this.props.onLogout}>
-            Sign out
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              CometRender
+            </Typography>
+            <Button color="inherit" onClick={this.props.onLogout}>
+              Sign out
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <TabContext value={this.state.tab}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList
+              onChange={(_ev, v) => {
+                this.setState({ tab: v });
+              }}
+            >
+              <Tab label="Status" value="status" />
+              <Tab label="Tasks manager" value="task-manager" />
+            </TabList>
+          </Box>
+          <TabPanel value="status">
+            <TasksStatus token={this.props.token} />
+          </TabPanel>
+          <TabPanel value="task-manager">TODO</TabPanel>
+        </TabContext>
+      </>
     );
   }
 }
---
+++ react-client/src/TasksStatus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactNode } from "react";
+
+export class TasksStatus extends React.Component<{ token: string }> {
+  constructor(p: any) {
+    super(p);
+  }
+  render(): ReactNode {
+    return <>tasks status</>;
+  }
+}
