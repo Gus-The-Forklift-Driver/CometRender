64052bab65c3c60a10a6aea78dc86a912d02bbd4
Pierre Hubert <pierre.git@communiquons.org>
2023-03-26 14:41:56 +0200

    Can create tasks

--- react-client/src/API.ts
+++ react-client/src/API.ts
@@ -6,7 +6,7 @@ export interface Task {
   uuid: string;
   name: string;
   blend_file: string;
-  render_size: number[];
+  render_size: [number, number];
   render_engine: string;
   view_layer: string;
   passes: string;
@@ -52,4 +52,39 @@ export class API {
 
     return await req.json();
   }
+
+  static async CreateTask(key: string, task: Task): Promise<void> {
+    task.uuid = generateUUID();
+
+    const req = await fetch(`${api_url}/new_task`, {
+      method: "POST",
+      headers: { key: key, "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    });
+
+    return await req.json();
+  }
+}
+
+function generateUUID(): string {
+  // Public Domain/MIT
+  var d = new Date().getTime(); //Timestamp
+  var d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0; //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
 }
--- react-client/src/TasksManager.tsx
+++ react-client/src/TasksManager.tsx
@@ -1,7 +1,12 @@
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import {
+  Button,
   CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   IconButton,
   Paper,
   Table,
@@ -13,17 +18,20 @@ import {
 } from "@mui/material";
 import React, { ReactNode } from "react";
 import { API, Task } from "./API";
+import { TaskEdit } from "./TaskEdit";
 
 export class TasksManager extends React.Component<
   { token: string },
-  { tasks?: Task[] }
+  { tasks?: Task[]; createDialog: boolean; currTask: Task; v: number }
 > {
   constructor(p: any) {
     super(p);
 
-    this.state = {};
+    this.state = { createDialog: false, currTask: {} as any, v: 0 };
 
     this.load = this.load.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+    this.createTask = this.createTask.bind(this);
   }
 
   componentDidMount() {
@@ -41,66 +49,110 @@ export class TasksManager extends React.Component<
     }
   }
 
+  closeDialog() {
+    this.setState({ createDialog: false });
+    this.load();
+  }
+
+  async createTask() {
+    try {
+      await API.CreateTask(this.props.token, this.state.currTask);
+      this.setState({ currTask: {} as any, createDialog: false });
+      this.load();
+      alert("Task created");
+    } catch (e) {
+      console.error(e);
+      alert("Failed to create task");
+    }
+  }
+
   render(): ReactNode {
     if (!this.state.tasks) return <CircularProgress />;
 
     return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Task name</TableCell>
-              <TableCell>Blend file</TableCell>
-              <TableCell>Engine</TableCell>
-              <TableCell>View layer</TableCell>
-              <TableCell>Passes</TableCell>
-              <TableCell>Frame step</TableCell>
-              <TableCell>Output path</TableCell>
-              <TableCell>Errors</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.tasks.map((t, idx) => (
-              <TableRow hover key={t.uuid}>
-                <TableCell component="th" scope="row">
-                  {t.name}
-                </TableCell>
-                <TableCell>{t.blend_file}</TableCell>
-                <TableCell>{t.render_engine}</TableCell>
-                <TableCell>{t.view_layer}</TableCell>
-                <TableCell>{t.passes}</TableCell>
-                <TableCell>{t.frame_step}</TableCell>
-                <TableCell>{t.output_path}</TableCell>
-                <TableCell>{JSON.stringify(t.errors)}</TableCell>
-                <TableCell>
-                  <IconButton
-                    disabled={idx === 0}
-                    onClick={() =>
-                      API.MoveTask(this.props.token, t, idx + 1)
-                        .then(this.load)
-                        .catch(handleErr)
-                    }
-                  >
-                    <ArrowUpwardIcon />
-                  </IconButton>
-
-                  <IconButton
-                    disabled={idx + 1 === this.state.tasks?.length}
-                    onClick={() =>
-                      API.MoveTask(this.props.token, t, idx - 1)
-                        .then(this.load)
-                        .catch(handleErr)
-                    }
-                  >
-                    <ArrowDownwardIcon />
-                  </IconButton>
-                </TableCell>
+      <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "end",
+          }}
+        >
+          <Button onClick={() => this.setState({ createDialog: true })}>
+            New task
+          </Button>
+        </div>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Task name</TableCell>
+                <TableCell>Blend file</TableCell>
+                <TableCell>Engine</TableCell>
+                <TableCell>View layer</TableCell>
+                <TableCell>Passes</TableCell>
+                <TableCell>Frame step</TableCell>
+                <TableCell>Output path</TableCell>
+                <TableCell>Errors</TableCell>
+                <TableCell></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {this.state.tasks.map((t, idx) => (
+                <TableRow hover key={t.uuid}>
+                  <TableCell component="th" scope="row">
+                    {t.name}
+                  </TableCell>
+                  <TableCell>{t.blend_file}</TableCell>
+                  <TableCell>{t.render_engine}</TableCell>
+                  <TableCell>{t.view_layer}</TableCell>
+                  <TableCell>{t.passes}</TableCell>
+                  <TableCell>{t.frame_step}</TableCell>
+                  <TableCell>{t.output_path}</TableCell>
+                  <TableCell>{JSON.stringify(t.errors)}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      disabled={idx === 0}
+                      onClick={() =>
+                        API.MoveTask(this.props.token, t, idx + 1)
+                          .then(this.load)
+                          .catch(handleErr)
+                      }
+                    >
+                      <ArrowUpwardIcon />
+                    </IconButton>
+
+                    <IconButton
+                      disabled={idx + 1 === this.state.tasks?.length}
+                      onClick={() =>
+                        API.MoveTask(this.props.token, t, idx - 1)
+                          .then(this.load)
+                          .catch(handleErr)
+                      }
+                    >
+                      <ArrowDownwardIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        {/* Create entry dialog */}
+        <Dialog open={this.state.createDialog} onClose={this.closeDialog}>
+          <DialogTitle>New task</DialogTitle>
+          <DialogContent>
+            <TaskEdit
+              t={this.state.currTask}
+              onChange={() => this.setState({ v: this.state.v + 1 })}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.createTask}>Create</Button>
+          </DialogActions>
+        </Dialog>
+      </>
     );
   }
 }
