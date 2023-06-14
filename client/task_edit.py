569264887cba66e9ed9307906af3633b62576e84
Gus-The-Forklift-Driver <benhubledingue@gmail.com>
2023-06-14 11:48:49 +0200

    removed unused settings

--- client/task_edit.py
+++ client/task_edit.py
@@ -47,14 +47,14 @@ def _help(message):
 def upload_task():
     task = {'task_name': dpg.get_value('task_name'),
             'blend_file': dpg.get_value('blend_file'),
-            'resolution_x': dpg.get_value('resolution_x'),
-            'resolution_y': dpg.get_value('resolution_y'),
-            'render_engine': dpg.get_value('render_engine'),
-            'scene': dpg.get_value('scene'),
-            'output_path': dpg.get_value('output_path'),
+            # 'resolution_x': dpg.get_value('resolution_x'),
+            # 'resolution_y': dpg.get_value('resolution_y'),
+            # 'render_engine': dpg.get_value('render_engine'),
+            # 'scene': dpg.get_value('scene'),
+            # 'output_path': dpg.get_value('output_path'),
             'frame_start': dpg.get_value('frame_start'),
             'frame_end': dpg.get_value('frame_end'),
-            'frame_step': dpg.get_value('frame_step'),
+            # 'frame_step': dpg.get_value('frame_step'),
             'chunks_size': dpg.get_value('chunks_size')
             }
     print(client.post_task(task))
@@ -93,6 +93,7 @@ with dpg.window(tag='main'):
             # New tasks settings
             dpg.add_input_text(label='Task name', tag='task_name')
             dpg.add_input_text(label='Blend file', tag='blend_file')
+            '''
             dpg.add_input_text(label='Scene', tag='scene')
             dpg.add_input_text(label='Output path', tag='output_path')
             with dpg.group(horizontal=True):
@@ -100,14 +101,15 @@ with dpg.window(tag='main'):
                 dpg.add_input_int(width=120, tag='resolution_y', default_value=1080)
                 dpg.add_text('resolution')
                 _help('resolution x/y')
+                '''
             with dpg.group(horizontal=True):
                 dpg.add_input_int(width=120, tag='frame_start', default_value=1)
                 dpg.add_input_int(width=120, tag='frame_end', default_value=250)
-                dpg.add_input_int(width=120, tag='frame_step', default_value=1)
+                # dpg.add_input_int(width=120, tag='frame_step', default_value=1)
                 dpg.add_text('frame range')
                 _help('frame range : start | end | step')
             dpg.add_input_int(label='Chunk size', tag='chunks_size', default_value=50)
-            dpg.add_combo(('CYCLES', 'BLENDER_EEVEE', 'BLENDER_WORKBENCH'), label='Render engine', default_value='CYCLES', tag='render_engine')
+            # dpg.add_combo(('CYCLES', 'BLENDER_EEVEE', 'BLENDER_WORKBENCH'), label='Render engine', default_value='CYCLES', tag='render_engine')
             dpg.add_button(label='Post task', callback=upload_task)
 
 # ===================================================
@@ -127,7 +129,7 @@ with dpg.window(tag='main'):
                                 dpg.bind_item_theme(dpg.last_item(), 'todo')
                             elif task[1] == 'running':
                                 dpg.bind_item_theme(dpg.last_item(), 'running')
-                            elif task[1] == 'done':
+                            elif task[1] == 'chunks_done':
                                 dpg.bind_item_theme(dpg.last_item(), 'done')
                             else:
                                 dpg.bind_item_theme(dpg.last_item(), 'error')
